c9dea2877c7063b9eb7c63ddd2b9c18fe409e35f
tfany <424047049@qq.com>
2019-04-24 17:25:57 +0800

    完善日志功能

--- src/main/java/com/huihui/o2o/controller/superadmin/AreaController.java
+++ src/main/java/com/huihui/o2o/controller/superadmin/AreaController.java
@@ -2,9 +2,10 @@ package com.huihui.o2o.controller.superadmin;
 
 import com.huihui.o2o.pojo.Area;
 import com.huihui.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +15,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger= LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
     @RequestMapping(value="/listarea",method= RequestMethod.GET)
     @ResponseBody
     private Map<String,Object> listArea(){
+        logger.info("=========start=======");
+        Long startTime=System.currentTimeMillis();
         Map<String,Object> modelMap =new HashMap<>();
         List<Area> list=new ArrayList<>();
         try {
@@ -33,6 +38,10 @@ public class AreaController {
             modelMap.put("success",false);
             modelMap.put("errMsg",e.toString());
         }
+        logger.error("test error!");
+        Long endTime=System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("========end========");
         return modelMap;
     }
 }
